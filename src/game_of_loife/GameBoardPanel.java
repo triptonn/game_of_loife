a3fb083cccca8151c7203e2ae206e4449539abb6
Nicolas Selig <triptonn.dev@gmail.com>
2025-03-14 23:22:54 +0100

    v0.1.05: WIP fixing bug in game logic --> see TODO

--- src/game_of_loife/GameBoardPanel.java
+++ src/game_of_loife/GameBoardPanel.java
@@ -7,6 +7,7 @@ import java.awt.event.*;
 public class GameBoardPanel extends JPanel implements Scrollable {
     private int[][] _board;
     private boolean painting;
+    private boolean isPlayerOne;
     private int _zoomFactor;
 
     public GameBoardPanel(int[][] board, int zoomFactor) {
@@ -17,11 +18,16 @@ public class GameBoardPanel extends JPanel implements Scrollable {
             @Override
             public void mousePressed(MouseEvent e) {
                 if (e.getButton() == MouseEvent.BUTTON1) {
-                	painting = true;
-                	updateBoard(e);
-                } else if (e.getButton() == MouseEvent.BUTTON2) {
-                	painting = true;
-                	updateBoard(e);
+                    painting = true;
+                    isPlayerOne = true;
+                    updateBoard(e, 1);
+                } else if (e.getButton() == MouseEvent.BUTTON3) {
+                    painting = true;
+                    isPlayerOne = false;
+                    updateBoard(e, 2);
+                } else {
+                    painting = true;
+                    updateBoard(e, 0);
                 }
             }
 
@@ -31,9 +37,9 @@ public class GameBoardPanel extends JPanel implements Scrollable {
 
             public void mouseDragged(MouseEvent e) {
                 if (painting && (e.getButton() == MouseEvent.NOBUTTON)) {
-                    updateBoard(e);
-                } else if (painting && (e.getButton() == MouseEvent.NOBUTTON)) {
-                	updateBoard(e);
+                    updateBoard(e, isPlayerOne ? 1 : 2, true);
+                } else if (painting) {
+                    updateBoard(e, 0, true);
                 }
             }
         };
@@ -101,24 +107,38 @@ public class GameBoardPanel extends JPanel implements Scrollable {
             for (int j = startCol; j < endCol; j++) {
                 int x = j * _zoomFactor;
                 int y = i * _zoomFactor;
-
-                g.setColor(_board[i][j] == 1 ? ColorScheme.GIRLY_MORE_ALIVE_COLOR : ColorScheme.DEAD_COLOR);
+                if (_board[i][j] > 0) {
+                    g.setColor(_board[i][j] == 1 ? ColorScheme.GIRLY_MORE_ALIVE_COLOR : ColorScheme.MORE_ALIVE_COLOR);
+                }
                 g.fillRect(x, y, _zoomFactor, _zoomFactor);
-
                 g.setColor(ColorScheme.GRID_COLOR);
                 g.drawRect(x, y, _zoomFactor, _zoomFactor);
             }
         }
     }
 
-    public void updateBoard(MouseEvent e) {
+    public int updateBoard(MouseEvent e, int player) {
+        return updateBoard(e, player, false);
+    }
+
+    public int updateBoard(MouseEvent e, int player, boolean dragging) {
         int col = e.getX() / _zoomFactor;
         int row = e.getY() / _zoomFactor;
 
         if (row >= 0 && row < _board.length && col >= 0 && col < _board[0].length) {
-            _board[row][col] = 1;
+            if (player == 1 && _board[row][col] == 1 && !dragging) {
+                _board[row][col] = 0;
+            } else if (player == 1) {
+                _board[row][col] = 1;
+            } else if (player == 2 && _board[row][col] == 2 && !dragging) {
+                _board[row][col] = 0;
+            } else {
+                _board[row][col] = 2;
+            }
             repaint();
+            return 0;
         }
+        return 1;
     }
 
     @Override
--- src/game_of_loife/GameLogic.java
+++ src/game_of_loife/GameLogic.java
@@ -1,12 +1,17 @@
 package game_of_loife;
 
 public class GameLogic {
+    private int scorePOne = 0;
+    private int scorePTwo = 0;
+    private String score = "";
+
     public void gameOfLife(int[][] board) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 encode(board, i, j);
             }
         }
+        System.out.println(this.score);
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
                 update(board, i, j);
@@ -15,7 +20,10 @@ public class GameLogic {
     }
 
     void encode(int[][] board, int i, int j) {
-        int aliveCount = 0;
+        int aliveCountPOne = 0;
+        int aliveCountPTwo = 0;
+        int aliveTotal = 0;
+        // System.out.println("val[" + i + "][" + j + "]: " + board[i][j]);
         for (int row = -1; row < 2; row++) {
             for (int column = -1; column < 2; column++) {
                 int neighR = i + row;
@@ -26,21 +34,83 @@ public class GameLogic {
 
                     int neighVal = board[neighR][neighC];
                     if (row == -1 || (row == 0 && column == -1)) {
+                        // System.out.println("neighVal[" + neighR + "][" + neighC + "]: " + neighVal);
+                        if (neighVal >= 200) {
+                            neighVal = neighVal % 200;
+                        } else if (200 > neighVal && neighVal >= 100) {
+                            neighVal = neighVal % 100;
+                        }
                         neighVal = neighVal / 10;
+                        // System.out.println("neighVal[" + neighR + "][" + neighC + "]: " + neighVal +
+                        // " / "
+                        // + board[neighR][neighC]);
                     }
 
                     if (neighVal == 1 && !(row == 0 && column == 0)) {
-                        aliveCount++;
+                        aliveCountPOne++;
+                    } else if (neighVal == 2 && !(row == 0 && column == 0)) {
+                        aliveCountPTwo++;
                     }
                 }
             }
         }
-        board[i][j] = board[i][j] * 10 + aliveCount;
+
+        // TODO: Somewhere here i need to count better
+        aliveTotal = aliveCountPOne + aliveCountPTwo;
+        // System.out.println("alive total: " + aliveTotal
+        // + " = aliveCountPOne: " + aliveCountPOne
+        // + " + aliveCountPTwo: " + aliveCountPTwo);
+        if ((aliveCountPOne == 0 && !(aliveCountPTwo == 0)) || (aliveCountPTwo == 0 && !(aliveCountPOne == 0))) {
+            if (aliveCountPOne < aliveCountPTwo) {
+                board[i][j] = 200 + board[i][j] * 10 + aliveTotal;
+            } else if (aliveCountPOne > aliveCountPTwo) {
+                board[i][j] = 100 + board[i][j] * 10 + aliveTotal;
+            } else {
+                double chance = Math.random();
+                if (chance <= 0.5) {
+                    board[i][j] = 100 + board[i][j] * 10 + aliveTotal;
+                } else {
+                    board[i][j] = 200 + board[i][j] * 10 + aliveTotal;
+                }
+            }
+        } else {
+            if (aliveCountPOne < aliveCountPTwo) {
+                board[i][j] = 200 + board[i][j] * 10 + aliveTotal;
+                scorePTwo++;
+            } else if (aliveCountPOne > aliveCountPTwo) {
+                board[i][j] = 100 + board[i][j] * 10 + aliveTotal;
+                scorePOne++;
+            } else {
+                double chance = Math.random();
+                if (chance <= 0.5) {
+                    board[i][j] = 100 + board[i][j] * 10 + aliveTotal;
+                    scorePOne++;
+                } else {
+                    board[i][j] = 200 + board[i][j] * 10 + aliveTotal;
+                    scorePTwo++;
+                }
+            }
+        }
+        this.score = "Score: " + String.valueOf(scorePOne) + ":" + String.valueOf(scorePTwo);
     }
 
     void update(int[][] board, int i, int j) {
+        int player = 0;
+        // System.out.println("board[" + i + "][" + j + "]: " + board[i][j]);
+
+        if (board[i][j] >= 200) {
+            board[i][j] = board[i][j] % 200;
+            player = 2;
+        } else if ((board[i][j] < 200) && (board[i][j] >= 100)) {
+            board[i][j] = board[i][j] % 100;
+            player = 1;
+        }
+
         int neighCount = board[i][j] % 10;
-        boolean isAlive = board[i][j] / 10 == 1;
+        // System.out.println("board[" + i + "][" + j + "]: " + board[i][j] + ",
+        // neighCount: " + neighCount);
+
+        boolean isAlive = (board[i][j] / 10) > 0;
 
         if ((isAlive && neighCount < 2) || (isAlive && neighCount > 3)) {
             board[i][j] = 0;
@@ -48,6 +118,10 @@ public class GameLogic {
             board[i][j] = 1;
         } else {
             board[i][j] = isAlive ? 1 : 0;
+        }
+
+        if (board[i][j] == 1 && player == 2) {
+            board[i][j] = 2;
         }
     }
 }
