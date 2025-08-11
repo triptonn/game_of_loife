03f39b06359e0fc85a28519d21e970518deef40b
Nicolas Selig <triptonn.dev@gmail.com>
2025-08-11 12:42:37 +0200

    v0.1.36: Split of display_scenes repo

--- core/interfaces/Attractor.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-import data.Vec;
-
-public interface Attractor {
-    Vec attract(Moveable m);
-}
--- core/interfaces/Informative.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import data.Vec;
-
-public interface Informative {
-    Vec getLocation();
-
-    void setLocation(Vec loc);
-}
