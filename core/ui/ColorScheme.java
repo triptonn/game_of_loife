669c344fac958a542b94843cdad59d37bd25e643
Nicolas Selig <triptonn.dev@gmail.com>
2025-08-12 15:23:18 +0200

    v0.1.37: Added ColorScheme back to the repo

---
+++ core/ui/ColorScheme.java
@@ -0,0 +1,12 @@
+package ui;
+
+import java.awt.Color;
+
+public class ColorScheme {
+    public static final Color MORE_ALIVE_COLOR = Color.decode("#14ff82");
+    public static final Color GIRLY_MORE_ALIVE_COLOR = Color.decode("#ff1493");
+    public static final Color GRID_COLOR = Color.DARK_GRAY;
+    public static final Color ALIVE_COLOR = Color.LIGHT_GRAY;
+    public static final Color DEAD_COLOR = Color.BLACK;
+    public static final Color WATER_COLOR = new Color(90, 188, 216, 64);
+}
